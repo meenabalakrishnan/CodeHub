958fe7a6e9def860c9d9070992ebcad98ef99cde
meenabalakrishnan <kb.meena@live.in>
2015-09-27 15:34:17 -0700

    Initial Commit

---
+++ DataStructures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by Meena Balakrishnan on 9/27/15.
+//  Copyright (c) 2015 Meena Balakrishnan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
